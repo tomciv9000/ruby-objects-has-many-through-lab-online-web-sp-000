12e9f0f40224a7d7f5efeed6097ad1677621cd97
Tom White <eugene.white@gmail.com>
2019-06-13 05:06:09 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist 
+  
+  attr_accessor :name
+  
+  @@all = []  
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_song(song_name, genre)
+    song = Song.new(song_name, self, genre)
+  end
+  
+  def songs 
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+  
+  def genres 
+    songs.map {|song| song.genre}
+  end
+
+end
