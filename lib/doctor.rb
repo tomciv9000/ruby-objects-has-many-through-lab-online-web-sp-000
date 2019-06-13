7aeb541ed776c051acb15b6e3e9d1d3541f922b7
Tom White <eugene.white@gmail.com>
2019-06-13 05:05:48 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -18,7 +18,7 @@ class Doctor
   end
   
   def appointments
-    Appointments.all.select do |appointment|
+    Appointment.all.select do |appointment|
       appointment.doctor == self
     end
   end
